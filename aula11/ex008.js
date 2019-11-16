1f1c2c06836cf52a1743c4a7756b6b2fd8f2ac71
Hadenia <hadenia@gmail.com>
2019-11-15 23:29:00 -0300

    alterando

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,13 +1,13 @@
-/*var vel = 60.5
+var vel = 60.5
 console.log(`A valocidade é ${vel} km/h`)
 if (vel > 60){
     console.log(`Você ultrapassou a valocidade permitida. MULTADO`)
 }
 
-//for(i = 1; i<=5; i++){
-//    console.log(`A valocidade é ${i} km/h`)
-//}
-*/
+for(i = 1; i<=5; i++){
+    console.log(`A valocidade é ${i} km/h`)
+}
+
 var pais = 'Brasil'
 if (pais == 'Brasil'){
     console.log('Brasileiro')
